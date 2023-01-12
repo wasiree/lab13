56cd301eb3ce1498f82bdaa98bac2a98a710f41b
Vasiree Nuntajan <wasiree098@gmail.com>
2023-01-12 22:35:30 +0700

    lab13_1 ok

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int  fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int  fibonacci(int x){
+    if(x == 0) return 0;
+    else if(x == 1) return 1;
+    else{
+        return fibonacci(x-1) + fibonacci(x-2);
+    }
 }
